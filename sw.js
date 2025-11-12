3d23d8637b4921bdd8fa488e90cd2b1814fbace3
CUSTOMWEBDEVS <116915610+CUSTOMWEBDEVS@users.noreply.github.com>
2025-11-11 18:14:09 -0600

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-// No-op SW to avoid 404, but we do NOT cache anything.
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", (e) => e.waitUntil(self.clients.claim()));
